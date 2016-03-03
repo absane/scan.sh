eced1d23bdbf54f60c427e905136b9b2a53f542d
root <jsibley1@gmail.com>
2016-03-03 15:22:12 -0500

    fixed install and .git issue

--- install.sh
+++ install.sh
@@ -2,6 +2,13 @@
 
 cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd
 
+rm -r includes/EyeWitness includes/Responder includes/yasuo includes/chuckle
+
+git clone https://github.com/ChrisTruncer/EyeWitness.git includes/EyeWitness
+git clone https://github.com/SpiderLabs/Responder.git includes/Responder
+git clone https://github.com/0xsauby/yasuo.git includes/yasuo
+#git clone https://github.com/nccgroup/chuckle.git includes/chuckle
+
 cd includes/EyeWitness/setup/
 ./setup.sh
 cd ../../../
