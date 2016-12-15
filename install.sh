8c821539cc5c19892afda3bdfee431a53d5c3a88
absane <jsibley1@gmail.com>
2016-12-15 18:20:25 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -19,6 +19,7 @@ mkdir -p /usr/share/rubygems-integration/all/gems/rake-10.3.2/bin/
 ln /usr/bin/rake /usr/share/rubygems-integration/all/gems/rake-10.3.2/bin/rake
 gem install ruby-nmap net-http-persistent mechanize text-table
 pip install xlwt
+pip install --upgrade html5lib==1.0b8
 clear
 echo -e 'INSTALLED! How to use\n*********************\n'
 ./scan.sh
