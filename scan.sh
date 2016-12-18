d2593e97550d43886865959305b957cf09d32e5f
absane <jsibley1@gmail.com>
2016-12-18 08:38:56 -0500

    Update scan.sh

--- scan.sh
+++ scan.sh
@@ -58,7 +58,7 @@ cd $HOME/includes/EyeWitness/
 ./EyeWitness.py -x $SCAN_RESULTS_LOCATION/$NETWORK.xml --rdp -d $SCAN_RESULTS_LOCATION/EyeWhitnessRDP --no-prompt --active-scan
 ./EyeWitness.py -x $SCAN_RESULTS_LOCATION/$NETWORK.xml --vnc -d $SCAN_RESULTS_LOCATION/EyeWhitnessVNC --no-prompt --active-scan
 mkdir -p $SCAN_RESULTS_LOCATION/EyeWhitness
-mv $SCAN_RESULTS_LOCATION/EyeWhitnessWeb $SCAN_RESULTS_LOCATION/EyeWhitnessRDP $SCAN_RESULTS_LOCATION/EyeWhitnessVNC $SCAN_RESULTS_LOCATION/EyeWhitness/
+mv $SCAN_RESULTS_LOCATION/EyeWhitnessWeb* $SCAN_RESULTS_LOCATION/EyeWhitnessRDP $SCAN_RESULTS_LOCATION/EyeWhitnessVNC $SCAN_RESULTS_LOCATION/EyeWhitness/
 cd $HOME
 
 #enum4linux
@@ -103,12 +103,12 @@ mv * $SCAN_RESULTS_LOCATION/Responder_data/
 cd $HOME
 #Brute Force attacks
 mkdir -p $SCAN_RESULTS_LOCATION/bruteforced_creds
-hydra -C $HOME/users+password.txt -M $SCAN_RESULTS_LOCATION/Parsed_Nmap_Results/Port-Files/21-TCP.txt ftp -t1 -o $SCAN_RESULTS_LOCATION/bruteforced_creds/ftp.txt
-hydra -C $HOME/users+password.txt -M $SCAN_RESULTS_LOCATION/Parsed_Nmap_Results/Port-Files/22-TCP.txt ssh -t1 -o $SCAN_RESULTS_LOCATION/bruteforced_creds/ssh.txt
-hydra -C $HOME/users+password.txt -M $SCAN_RESULTS_LOCATION/Parsed_Nmap_Results/Port-Files/3306-TCP.txt mysql -t1 -o $SCAN_RESULTS_LOCATION/bruteforced_creds/mysql.txt
-hydra -C $HOME/users+password.txt -M $SCAN_RESULTS_LOCATION/Parsed_Nmap_Results/Port-Files/1443-TCP.txt mssql -t1 -o $SCAN_RESULTS_LOCATION/bruteforced_creds/mssql.txt
-hydra -C $HOME/users+password.txt -M $SCAN_RESULTS_LOCATION/Parsed_Nmap_Results/Port-Files/3389-TCP.txt rdp -t1 -o $SCAN_RESULTS_LOCATION/bruteforced_creds/rdp.txt
-hydra -C $HOME/users+password.txt -M $SCAN_RESULTS_LOCATION/Parsed_Nmap_Results/Port-Files/23-TCP.txt telnet -t1 -o $SCAN_RESULTS_LOCATION/bruteforced_creds/telnet.txt
+hydra -C $HOME/users+password.txt -M $SCAN_RESULTS_LOCATION/Parsed_Nmap_Results/Port-Files/21-TCP.txt ftp -t1 -W1 -o $SCAN_RESULTS_LOCATION/bruteforced_creds/ftp.txt
+hydra -C $HOME/users+password.txt -M $SCAN_RESULTS_LOCATION/Parsed_Nmap_Results/Port-Files/22-TCP.txt ssh -t1 -W1 -o $SCAN_RESULTS_LOCATION/bruteforced_creds/ssh.txt
+hydra -C $HOME/users+password.txt -M $SCAN_RESULTS_LOCATION/Parsed_Nmap_Results/Port-Files/3306-TCP.txt mysql -t1 -W1 -o $SCAN_RESULTS_LOCATION/bruteforced_creds/mysql.txt
+hydra -C $HOME/users+password.txt -M $SCAN_RESULTS_LOCATION/Parsed_Nmap_Results/Port-Files/1443-TCP.txt mssql -t1 -W1 -o $SCAN_RESULTS_LOCATION/bruteforced_creds/mssql.txt
+hydra -C $HOME/users+password.txt -M $SCAN_RESULTS_LOCATION/Parsed_Nmap_Results/Port-Files/3389-TCP.txt rdp -t1 -W1 -o $SCAN_RESULTS_LOCATION/bruteforced_creds/rdp.txt
+hydra -C $HOME/users+password.txt -M $SCAN_RESULTS_LOCATION/Parsed_Nmap_Results/Port-Files/23-TCP.txt telnet -t1 -W1 -o $SCAN_RESULTS_LOCATION/bruteforced_creds/telnet.txt
 
 #Open results in IceWeasel
 firefox &
