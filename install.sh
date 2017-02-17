b2e12c7cc0cfbb27be7688a98b615a6147f9fa7b
absane <jsibley1@gmail.com>
2017-02-17 17:04:16 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -9,6 +9,7 @@ apt-get install xsltproc firewalk -y
 git clone https://github.com/ChrisTruncer/EyeWitness.git includes/EyeWitness
 git clone https://github.com/lgandx/Responder.git includes/Responder
 git clone https://github.com/0xsauby/yasuo.git includes/yasuo
+git clone https://github.com/portcullislabs/udp-proto-scanner.git includes/udp-proto-scanner
 #git clone https://github.com/nccgroup/chuckle.git includes/chuckle
 
 mv phantomjs includes/EyeWitness/ 
@@ -20,6 +21,6 @@ ln /usr/bin/rake /usr/share/rubygems-integration/all/gems/rake-10.3.2/bin/rake
 gem install ruby-nmap net-http-persistent mechanize text-table
 pip install xlwt
 pip install --upgrade html5lib==1.0b8
-clear
-echo -e 'INSTALLED! How to use\n*********************\n'
+#clear
+echo -e '\n\nINSTALLED! How to use\n*********************\n'
 ./scan.sh
