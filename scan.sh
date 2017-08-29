eb9437d740379630fefb7823e4da3892b92b229c
absane <jsibley1@gmail.com>
2017-08-29 08:19:36 -0400

    Update scan.sh

--- scan.sh
+++ scan.sh
@@ -34,11 +34,11 @@ nmap -e $INTERFACE -v --open -T4 -Pn -n -sU -p53,69,111,123,137,161,500,514,520 
 grep Host /tmp/*.gnmap | awk '{print $2}' | sort | uniq > $HOME/$NETWORK
 
 #UDP Scan on live hosts
-nmap -e $INTERFACE --randomize-hosts -iL $HOME/$NETWORK -v -T4 -sC --open -Pn -n -sU -sV -oA $SCAN_RESULTS_LOCATION/${NETWORK}_udp -p53,67-69,11,123,135,137-139,161-162,445,500,514,520,631,996-999,1434,1701,1900,3283,4500,5353,49152-49154
+nmap -e $INTERFACE --randomize-hosts -iL $HOME/$NETWORK -v -T4 --open -Pn -n -sU -sV -oA $SCAN_RESULTS_LOCATION/${NETWORK}_udp --source-port=53 --script=default,safe -p53,67-69,11,123,135,137-139,161-162,445,500,514,520,631,996-999,1434,1701,1900,3283,4500,5353,49152-49154
 
 
 #TCP Scan on live hosts
-nmap -e $INTERFACE --randomize-hosts -iL $HOME/$NETWORK -v -T4 -sC --open -Pn -n -sT -sV -oA $SCAN_RESULTS_LOCATION/$NETWORK -p-
+nmap -e $INTERFACE --randomize-hosts -iL $HOME/$NETWORK -v -T4 --open -Pn -n -sT -sV -oA $SCAN_RESULTS_LOCATION/$NETWORK --source-port=20 --script=default,safe -p-
 
 #Create HTML of Nmap Scan Results
 xsltproc $SCAN_RESULTS_LOCATION/$NETWORK.xml -o $SCAN_RESULTS_LOCATION/$NETWORK.html
